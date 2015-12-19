13e19cb59565eac659af79308283e423875c477a
lm2343635 <lm2343635@126.com>
2015-12-19 12:49:38 +0900

    modify 27-2

--- 27-2.c
+++ 27-2.c
@@ -10,17 +10,29 @@
 
 #define N 5
 
-char a[N][N];
+char a[N][N]={
+    {0, 0, 0, 1, 0},
+    {1, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0},
+    {1, 0, 1, 0, 0}
+};
+
 int sorted[N], indegree[N], nodestack[N], stackp=0;
+
+void print_array(int a[], int n) {
+    for(int i=0;i<N;i++) 
+       printf("%d ", a[i]);
+   printf("\n");
+}
 
 void count_indegree() {
     int tail, head;
     for(head=0; head<N; head++) {
         indegree[head]=0;
         for(tail=0; tail<N; tail++) {
-            if(a[head][tail]==1) {
+            if(a[head][tail]==1)
                 indegree[head]++;
-            }
         }
     }
 }
@@ -57,22 +69,7 @@ int sort(int sorted[]) {
     return count;
 }
 
-//int main(int argc, const char * argv[]) {
-//    char b[N][N]={
-//        {0, 0, 0, 1, 0},
-//        {1, 0, 0, 0, 0},
-//        {0, 0, 0, 0, 0},
-//        {0, 0, 0, 0, 0},
-//        {1, 0, 1, 0, 0}
-//    };
-//    for(int i=0;i<N;i++) {
-//        for(int j=0;j<N;j++) {
-//            a[i][j]=b[i][j];
-//        }
-//    }
-//    printf("%d\n", sort(sorted));
-//    for(int i=0;i<N;i++) {
-//        printf("%d ", sorted[i]);
-//    }
-//
-//}
+int main(int argc, const char * argv[]) {
+   printf("%d\n", sort(sorted));
+   print_array(sorted, N);
+}
